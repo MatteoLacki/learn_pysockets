6780f6307a82584b3bc9f3bbbe05fcbc6aa1d213
MatteoLacki <matteo.lacki@gmail.com>
2019-08-22 11:02:36 +0200

    Finished vid2

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
 import socket
+import sys
+
+port = int(sys.argv[1])
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 12342))
+s.connect((socket.gethostname(), port))
 
-full_msg = []
+full_msg = ""
+new_msg = True
 while True:
-	msg = s.recv(8)
-	if len(msg) <= 0:
-		break
-	full_msg.append(msg.decode("utf-8"))
-	print(msg, len(msg))
-full_msg = "".join(full_msg)
-print(full_msg)
+    msg = s.recv(16)
+
+    if new_msg:
+        print(f"new message length: {msg[:HEADERSIZE]}")
+        msglen = int(msg[:HEADERSIZE])
+        new_msg = False
+    
+    full_msg += msg.decode("utf-8")
+
+    if len(full_msg) - HEADERSIZE == msglen:
+        print(f"Full message: {full_msg[HEADERSIZE:]}")
+        full_msg = ""
+        new_msg = True
--- server.py
+++ server.py
@@ -1,12 +1,24 @@
 import socket
+import time
+
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 12342))
+s.bind((socket.gethostname(), 0)) #0 will select the first open port.
+print("ip {}\tport {}".format(*s.getsockname()))
 s.listen(5)
 
-
 while True:
-	clientsocket, address = s.accept()
-	print(f"Connection from {address} established.")
-	clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-	clientsocket.close()
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} established.")
+    msg = "Welcome to the server!"
+    msg = f"{len(msg):<{HEADERSIZE}}" + msg
+    clientsocket.send(bytes(msg, "utf-8"))
+    # clientsocket.close()
+
+    while True:
+        time.sleep(1)
+        msg = f"The time is: {time.time()}"
+        print(f'Sending "{msg}" to {address[1]}.')
+        msg = f"{len(msg):<{HEADERSIZE}}" + msg
+        clientsocket.send(bytes(msg, "utf-8"))
